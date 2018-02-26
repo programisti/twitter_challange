d8a0c15710d911132a430b202acd0b4ac0235be6
Zaali Kavelashvili <zaali@live.com>
2018-02-26 16:43:09 +0400

    Save hashtag on DB

--- lib/twitter.ex
+++ lib/twitter.ex
@@ -8,31 +8,36 @@ defmodule Twitter do
   end
 
   def subscribe(hashtag) when is_binary(hashtag) do
+    IO.inspect "------- STARTED for #{hashtag} ---------"
     Task.async(fn -> search(hashtag) end)
     |> fetch
   end
 
   defp fetch(tasks) do
     Enum.each(tasks, fn(task) ->
-      task |> Task.await |> save
+      task |> Task.await
     end)
   end
 
   defp search(hashtag) do
+    IO.inspect "-------- subscribing to #{hashtag} ------------"
     ExTwitter.search(hashtag, [count: 5])
     |> Enum.map(fn(tweet) -> %{"text" => tweet.text, "author" => tweet.user.screen_name} end)
+    |> save(hashtag)
   end
 
-  defp save(tweets) do
+  defp save(tweets, hashtag) do
     tweets
     |> Enum.map(fn(%{"author" => author, "text" => text}) ->
+      IO.inspect "-------- Saving to DB hashTag: #{hashtag} text: #{String.slice(text, 0..25)}"
       Sqlitex.with_db('db.sqlite3', fn(db) ->
         Sqlitex.query(
           db,
-          "INSERT INTO tweets (text, author) VALUES ($1, $2)",
-          bind: [text, author])
+          "INSERT INTO tweets (text, author, hashtag) VALUES ($1, $2, $3)",
+          bind: [text, author, hashtag])
       end)
     end)
+    IO.inspect "-------- SAVING TO DB DONE for hashtag: #{hashtag}------------"
 
   end
 end
